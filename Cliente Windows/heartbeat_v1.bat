43ab5d8b2cfcf4282225d422dd52c071b870fb27
albertor02 <jose_docs@hotmail.com>
2020-04-07 23:10:06 -0500

    Add files via upload

---
+++ Cliente Windows/heartbeat_v1.bat
@@ -0,0 +1,15 @@
+@echo off
+rem "Heartbeat v1 - Alberto Rivadeneira"
+rem "Modificar direccion ip en la variable SRV "
+rem "Modificar subred en la consulta findstr "
+rem   "ejemplo (subred 192.168.1.0/24) -> findstr 192.168.10.0)"
+
+
+setlocal ENABLEEXTENSIONS & set "i=0.0.0.0" & set "j="
+for /f "tokens=4" %%a in ('route print^|findstr 10.20.30.0') do (
+  if not defined j for %%b in (%%a) do set "i=%%b" & set "j=1")
+endlocal & set "dip=%i%"
+
+set "SRV=192.168.1.38"
+
+curl -i -s --connect-timeout 5 "http://%SRV%/repo/reg.php?host=%computername%&ip=%dip%&username=%username%"
