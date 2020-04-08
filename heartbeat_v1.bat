854a9b0a63c0a91a608051cec8fe142811940c98
albertor02 <jose_docs@hotmail.com>
2020-04-07 23:01:14 -0500

    Update heartbeat_v1.bat

--- heartbeat_v1.bat
+++ heartbeat_v1.bat
@@ -1,5 +1,5 @@
 @echo off
-rem "Heartbeat v1 - Alberto Rivadeneira"
+rem "Heartbeat v1 - 4r2"
 rem "Modificar direccion ip en la variable SRV "
 rem "Modificar subred en la consulta findstr "
 rem   "ejemplo (subred 192.168.1.0/24) -> findstr 192.168.10.0)"
@@ -10,6 +10,6 @@ for /f "tokens=4" %%a in ('route print^|findstr 10.20.30.0') do (
   if not defined j for %%b in (%%a) do set "i=%%b" & set "j=1")
 endlocal & set "dip=%i%"
 
-set "SRV=192.168.1.38"
+set "SRV=192.168.xx.xx"
 
-curl -i -s --connect-timeout 5 "http://%SRV%/repo/reg.php?host=%computername%&ip=%dip%&username=%username%"
+curl -i -s --connect-timeout 5 "http://%SRV%/reg.php?host=%computername%&ip=%dip%&username=%username%"
